26c7a118c9b6a22d8f00e177ba2700db51de75b5
Néstor Martínez <nestor.marvi@gmail.com>
2021-09-05 19:44:47 -0300

    Explicando por qué la clase no cumple con el principio SRP.

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -18,6 +18,14 @@ namespace SRP
             this.Codigo = codigo;
         }
 
+        /*
+        Si quisiéramos agregarle a la clase libro la capacidad de registrar si es una edición especial 
+        del mismo, tendríamos que modificar la clase "Libro". Y si quisiéramos cambiar el tipo
+        de almacenamiento de físico a digital, también habría que cambiar a la clase. Debido a esto, 
+        la clase "Libro" no cumple con las normas SRP y por ende habría que hacer que el método 
+        "AlmacenarLibro" sea una clase separada que colabore con la clase "Libro". 
+        */
+        
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
